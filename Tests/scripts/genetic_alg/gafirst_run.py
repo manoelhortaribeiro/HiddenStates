11e29025c0654281d3cf4de59f1db55b92b4dc46
Manoel <manoelhortaribeiro@gmail.com>
2015-10-24 18:37:12 -0200

    ga ready to go!

--- Tests/scripts/genetic_alg/gafirst_run.py
+++ Tests/scripts/genetic_alg/gafirst_run.py
@@ -8,17 +8,18 @@ n_labels, folds, path, data, label, train, test, name, fold, date, project_folde
 description = "run0_CAD120_GA_seed1"
 
 init = 6
-p_size = 10
+p_size = 15
 CXPB = 0.6
 MUTPB = 0.2
-NGEN = 20
-t_size = 1
+NGEN = 15
+t_size = 3
 seed = 1
 svm = 5
+elite_size = 1
 
 tests, logbook, best = ga.main(n_labels, folds, path, data, label, train, test, name, fold,
                                svm=svm, init=init, p_size=p_size, CXPB=CXPB, MUTPB=MUTPB, NGEN=NGEN,
-                               t_size=t_size, seed=seed)
+                               t_size=t_size, seed=seed, elite_size=elite_size)
 
 aux.write_file(project_folder, out, description, date, tests, logbook, best, svm)
 
