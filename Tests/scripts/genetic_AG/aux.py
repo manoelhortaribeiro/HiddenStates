75ad8a8bdbc6c41937fb802dad70d9ddaf42757b
Manoel <manoelhortaribeiro@gmail.com>
2015-11-21 13:50:30 -0200

    created folds with less data

--- Tests/scripts/genetic_AG/aux.py
+++ Tests/scripts/genetic_AG/aux.py
@@ -18,20 +18,19 @@ def armgesture():
     return datapath, n_labels, folds, path, data, label, train, test, name, fold, date, project_folder, out
 
 
-
 def armgesture2():
     n_labels = 6
     folds = [1, 2, 3]
-    path = "/home/bruno.teixeira/distance6/hidden_states/Dataset/Data/ArmGestureContinuous/"
+    path = "/home/manoel/Projects/hidden_states_entropy/Dataset/Data/ArmGestureContinuous3FoldHALF/"
     data = "data"
     label = "seqLabels"
     train = "Train"
     test = "Test"
     name = "ArmGestureContinuous"
-    fold = "Fold"
+    fold = "FoldHALF"
     date = datetime.datetime.utcnow().strftime("%d_%m_%y-%H:%M")
-    project_folder = "/home/bruno.teixeira/distance1/"
-    out = "hidden_states/Dataset/Output/Results/"
+    project_folder = "/home/manoel/Projects/"
+    out = "hidden_states_entropy/Dataset/Output/Results/"
     datapath = path + "/ArmGestureDiscrete.mat"
 
     return datapath, n_labels, folds, path, data, label, train, test, name, fold, date, project_folder, out
@@ -53,7 +52,7 @@ def write_file(project_folder, out, description, date, tests, logbook, best, svm
     for b in best:
         f.write("\n|   " + str(b[0]) + "        " + str(b[1]) + "         " + str(b[2])[1:9])
 
-    f.write("\nARBITRARY: ")
+    f.write("\nVALIDATION\nARBITRARY, OURS: ")
     f.write(str(arbitrary))
     f.close()
 
