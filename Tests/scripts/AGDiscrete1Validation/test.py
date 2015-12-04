537030beb57c66a5a2ed6bc8e65d5ca1bb64677a
Manoel <manoelhortaribeiro@gmail.com>
2015-12-04 11:31:30 -0200

    created folds with less data

---
+++ Tests/scripts/AGDiscrete1Validation/test.py
@@ -0,0 +1,54 @@
+import Tests.hsparallel_alt as hs
+import aux
+
+__author__ = 'Manoel Ribeiro'
+
+tests = [8, 7, 6]
+
+svmiter = 7
+seed = 3
+n_jobs = 5
+
+description = "ag3_7i3EUCLIDIANsc3_t6-48"
+
+detailed = "Full random init, seed 3, jobs 4, svmiter 7"
+
+datapath, n_labels, folds, path, data, label, train, test, name, fold, date, project_folder, out = aux.armgesturethreefold()
+
+opt_tests_avg_std, opt_trains_avg_std, sopt_tests_avg_std, sopt_trains_avg_std = \
+    hs.eval_data_set(tests, n_labels, folds, path, data, label, train, test, name, fold, kind=0.3,
+                     svmiter=svmiter, seed=seed, n_jobs=n_jobs, subopt=False, datapath=datapath,
+                     measure="sqeuclidian")
+
+aux.write_file(project_folder, out, description, date, svmiter, tests, opt_tests_avg_std,
+               opt_trains_avg_std, sopt_tests_avg_std, sopt_trains_avg_std, detailed)
+
+
+description = "ag3_7i3COSINEsc3_t6-48"
+
+detailed = "Full random init, seed 3, jobs 4, svmiter 7"
+
+datapath, n_labels, folds, path, data, label, train, test, name, fold, date, project_folder, out = aux.armgesturethreefold()
+
+opt_tests_avg_std, opt_trains_avg_std, sopt_tests_avg_std, sopt_trains_avg_std = \
+    hs.eval_data_set(tests, n_labels, folds, path, data, label, train, test, name, fold, kind=0.3,
+                     svmiter=svmiter, seed=seed, n_jobs=n_jobs, subopt=False, datapath=datapath,
+                     measure="cosine")
+
+aux.write_file(project_folder, out, description, date, svmiter, tests, opt_tests_avg_std,
+               opt_trains_avg_std, sopt_tests_avg_std, sopt_trains_avg_std, detailed)
+
+description = "ag3_7i3CORRELATIONsc3_t6-48"
+
+detailed = "Full random init, seed 3, jobs 4, svmiter 7"
+
+datapath, n_labels, folds, path, data, label, train, test, name, fold, date, project_folder, out = aux.armgesturethreefold()
+
+opt_tests_avg_std, opt_trains_avg_std, sopt_tests_avg_std, sopt_trains_avg_std = \
+    hs.eval_data_set(tests, n_labels, folds, path, data, label, train, test, name, fold, kind=0.3,
+                     svmiter=svmiter, seed=seed, n_jobs=n_jobs, subopt=False, datapath=datapath,
+                     measure="correlation")
+
+aux.write_file(project_folder, out, description, date, svmiter, tests, opt_tests_avg_std,
+               opt_trains_avg_std, sopt_tests_avg_std, sopt_trains_avg_std, detailed)
+
